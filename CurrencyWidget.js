9d5e1df1283f12c5fd95857df6b0bd4ce75d5c0b
Rohit Chouhan <34239087+rohit-chouhan@users.noreply.github.com>
2023-03-07 11:42:34 +0530

    version 1.0.1 released
    - Dropdown list has been changed to Input
    - getCalculatedRate() function added

--- CurrencyWidget.js
+++ CurrencyWidget.js
@@ -1,61 +1,83 @@
 (function () {
-  let template = document.createElement("template");
-  template.innerHTML = `
-      <style>
-        :host {}
-      </style>
-      <div>
-        <p>Current Conversion Rate: <span id="conversionRate"></span></p>
-      </div>
-    `;
+   let template = document.createElement("template");
+   template.innerHTML = `
+       <style>
+         :host {}
+       </style>
+       <div>
+         <p>Current Conversion Rate: <span id="conversionRate"></span></p>
+       </div>
+     `;
 
-  class CurrencyWidget extends HTMLElement {
-    constructor() {
-      super();
-      let shadowRoot = this.attachShadow({
-        mode: "open"
-      });
-      shadowRoot.appendChild(template.content.cloneNode(true));
-      this._props = {};
+   class CurrencyWidget extends HTMLElement {
+      constructor() {
+         super();
+         let shadowRoot = this.attachShadow({
+            mode: "open"
+         });
+         shadowRoot.appendChild(template.content.cloneNode(true));
+         this._props = {};
+      }
+
+      async connectedCallback() {
+         this.updateConversionRate();
+         this.interval = setInterval(() => {
+            this.updateConversionRate();
+         }, 4000);
+      }
+
+      disconnectedCallback() {
+         clearInterval(this.interval);
+      }
+
+      async updateConversionRate() {
+         const from = this._props.from || "USD";
+         const to = this._props.to || "INR";
+         const amount = this._props.amount || 1;
+         const decimalPlaces = this._props.decimalPlaces || 2;
+         //const price= this._props.amountprice|| 0;
+
+         const url = `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`;
+         const response = await fetch(url);
+         const data = await response.json();
+
+         const conversionRate = data.result.toFixed(decimalPlaces);
+         const conversionRateElement = this.shadowRoot.getElementById("conversionRate");
+         conversionRateElement.innerText = `${conversionRate} ${to}/${from}`;
+         const getprice = conversionRate.toString();
+this._props._amountprice = getprice;
+this.setAmount(getprice);
+//console.log(this._props.price);
+      }
+
+onCustomWidgetBeforeUpdate(changedProperties) {
+  this._props = {
+    ...this._props,
+    ...changedProperties
+  };
+//console.log(this._props);
+  // this._props["amountprice"] = this._props["_amountprice"];
+  //changedProperties["amountprice"] = "superman";
+
+}
+
+setAmount(price){
+  const amountprice = price;
+
+  this.dispatchEvent(new CustomEvent("propertiesChanged", {
+    detail: {
+      properties: {
+        amountprice: amountprice
+      }
     }
+  }));
+}
 
-    async connectedCallback() {
-      this.updateConversionRate();
-      this.interval = setInterval(() => {
-        this.updateConversionRate();
-      }, 4000);
-    }
+      onCustomWidgetAfterUpdate(changedProperties) {
+        
+      
+      }
+   }
 
-    disconnectedCallback() {
-      clearInterval(this.interval);
-    }
-
-    async updateConversionRate() {
-      const from = this._props.from || "USD";
-      const to = this._props.to || "INR";
-      const amount = this._props.amount || 1 ;
-      const decimalPlaces = this._props.decimalPlaces || 2;
-
-      const url = `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`;
-      const response = await fetch(url);
-      const data = await response.json();
-
-      const conversionRate = data.result.toFixed(decimalPlaces);
-      const conversionRateElement = this.shadowRoot.getElementById("conversionRate");
-      conversionRateElement.innerText = `${conversionRate} ${to}/${from}`;
-    }
-
-    onCustomWidgetBeforeUpdate(changedProperties) {
-      this._props = {
-        ...this._props,
-        ...changedProperties
-      };
-    }
-
-    onCustomWidgetAfterUpdate(changedProperties) {
-      console.warn(changedProperties);
-		}
-  }
-
-  customElements.define("com-rohitchouhan-currencywidget", CurrencyWidget);
+   customElements.define("com-rohitchouhan-sap-currencywidget", CurrencyWidget);
 })();
---
+++ Readme.md
@@ -0,0 +1,42 @@
+
+
+<img src="https://raw.githubusercontent.com/SAP-Custom-Widget/CurrencyWidget/main/icon.png" width="100">
+
+## SAP Custom Widget: Currency Widget
+This is a custom widget that displays the real-time currency conversion rate between two currencies. It can be used in SAP applications to enhance user experience by providing quick access to currency conversion information.
+
+![preview](https://raw.githubusercontent.com/SAP-Custom-Widget/CurrencyWidget/main/screenshot.png)
+
+## Installation
+To use this widget in your SAP application, follow these steps:
+
+- Download the `CurrencyWidget.json` file from the URLs specified in the webcomponents property of the JSON.
+- Go to your SAC Portal, select Analytic Application from the left side bar, and then go to the Custom Widget tab.
+- Click on the + icon on the right side and select the `CurrencyWidget.json` file that you downloaded.
+- You're done! You can now use it in your app.
+
+## Methods
+The widget has the following methods:
+
+### Set Methods
+
+|  Method | Parameter Type  | Description  |
+| ------------ | ------------ | ------------ |
+| setFrom(from) | string |  Set Source Currency Code |
+| setTo(to) | string |  Set Target Currency Code |
+| setAmount(amount) | integer |  Set Amount to Calculate |
+| setDecimalPlaces(decimalPlaces) | integer |  Set Decimal Values |
+
+### get Methods
+
+|  Method | Return Type | Description  |
+| ------------ | ------------ | ------------ |
+| getFrom()  | string |  return Source Currency Code |
+| getTo()  | string |  return Target Currency Code |
+| getAmount()  | integer |  return Amount to Calculate |
+| getDecimalPlaces()  | integer |  return Decimal Values |
+| getCalculatedRate()  | string |  return Calculated Currency Rate |
+
+## About
+This widget is developed by [Rohit Chouhan](http://linkedin.com/in/itsrohitchouhan "Rohit Chouhan")
+
